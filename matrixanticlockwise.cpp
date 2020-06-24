a1fe86161b5b5e4931fa6f61338379aee40aaa07
chandushivalingareddy <53473880+chandushivalingareddy@users.noreply.github.com>
2020-06-24 13:14:12 +0530

    Add files via upload

---
+++ matrixanticlockwise.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+#define N 3
+using namespace std;
+
+//Function to reverse the columns of the matrix
+void reverse_column(int arr[N][N])
+{
+    int k;
+    for (int i = 0; i < N; i++)
+    {
+        k = N-1;
+        for (int j = 0; j < k; j++)
+        {
+            swap(arr[j][i], arr[k][i]);
+            k--;
+        }}}
+
+// Transpose of a matrix
+void transpose(int arr[N][N])
+{
+    for (int i = 0; i < N; i++)
+        for (int j = i; j < N; j++)
+            swap(arr[i][j], arr[j][i]);
+}
+
+// print the matrix
+void print_matrix(int mat[N][N])
+{
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+            printf("%d\t", mat[i][j]);
+
+        printf("\n");
+    }
+    printf("\n");
+}
+
+// Main function
+int main()
+{
+    int mat[N][N] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    cout<<"The matrix before rotation\n";
+    print_matrix(mat);
+    transpose(mat);
+    cout<<"The matrix after rotation - anticlockwise\n";
+    reverse_column(mat);
+    print_matrix(mat);
+    return 0;
+}
