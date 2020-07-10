787330483394b408f2b202b4c97414e526754205
chandushivalingareddy <53473880+chandushivalingareddy@users.noreply.github.com>
2020-07-10 14:27:36 +0530

    Add files via upload

--- count.c
+++ count.c
@@ -1,20 +1,36 @@
-int count(int n) 
-{ 
-    if (n < 3) 
-    return n; 
-    if (n >= 3 && n < 10) 
-    return n-1; 
-    int po = 1; 
-    while (n/po > 9) 
-    po = po*10; 
-    int msd = n/po; 
-    if (msd != 3) 
-      return count(msd)*count(po - 1) + count(msd) + count(n%po); 
-    else
-       return count(msd*po - 1); 
-} 
+#include <stdio.h>
 int main() 
-{ 
-    printf ("%d ", count(578)); 
-    return 0; 
-} 
+{
+    char line[150];
+    int vowels, consonant, digit, space;
+    vowels = consonant = digit = space = 0;
+    printf("Enter a line of string: ");
+    fgets(line, sizeof(line), stdin);
+    for (int i = 0; line[i] != '\0'; ++i) 
+    {
+        if (line[i] == 'a' || line[i] == 'e' || line[i] == 'i' ||
+            line[i] == 'o' || line[i] == 'u' || line[i] == 'A' ||
+            line[i] == 'E' || line[i] == 'I' || line[i] == 'O' ||
+            line[i] == 'U') 
+        {
+            ++vowels;
+        } 
+        else if ((line[i] >= 'a' && line[i] <= 'z') || (line[i] >= 'A' && line[i] <= 'Z')) 
+        {
+            ++consonant;
+        } 
+        else if (line[i] >= '0' && line[i] <= '9') 
+        {
+            ++digit;
+        }  
+        else if (line[i] == ' ') 
+        {
+            ++space;
+        }
+    }
+    printf("Vowels: %d", vowels);
+    printf("\nConsonants: %d", consonant);
+    printf("\nDigits: %d", digit);
+    printf("\nWhite spaces: %d", space);
+    return 0;
+}
